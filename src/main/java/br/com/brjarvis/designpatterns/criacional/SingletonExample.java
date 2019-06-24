504c382c752dd2c2710889f6656a915128acbf51
Leandro Araújo <leandroaraujo_sp@hotmail.com>
2019-06-23 23:20:34 -0300

    Renomeada classe Singleton.

--- src/main/java/br/com/brjarvis/designpatterns/criacional/SingletonExample.java
+++ src/main/java/br/com/brjarvis/designpatterns/criacional/SingletonExample.java
@@ -3,13 +3,13 @@ package br.com.brjarvis.designpatterns.criacional;
 /**
  * Constrói uma instância única para o projeto inteiro.
  */
-public class Singleton {
-    private static Singleton instance;
+public class SingletonExample {
+    private static SingletonExample instance;
 
     /**
      * Construtor necessário para impedir criação de outras instâncias.
      */
-    private Singleton() {
+    private SingletonExample() {
         super();
     }
 
@@ -18,14 +18,14 @@ public class Singleton {
      *
      * @return a instância única no projeto
      */
-    public static Singleton getInstance() {
+    public static SingletonExample getInstance() {
         if (instance == null)
-            instance = new Singleton();
+            instance = new SingletonExample();
         return instance;
     }
 
     @Override
     public String toString() {
-        return "Singleton{}";
+        return "SingletonExample{}";
     }
 }
